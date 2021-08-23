2365becf6bf23c304a5cefc3f250fe0dccfe8de0
Brian Kozub <brikozub@gmail.com>
2021-08-22 18:16:50 -0600

    Creating a Base Set of Screens with React Native

--- utils/Router.tsx
+++ utils/Router.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import { createAppContainer, createSwitchNavigator } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
+import AccountsScreen from "../screens/AccountsScreen";
 
 import FeedScreen from "../screens/FeedScreen";
+import PostFormScreen from "../screens/PostFormScreen";
 import SearchScreen from "../screens/SearchScreen";
 
 const AppStack = createStackNavigator(
 	{
 		Feed: FeedScreen,
 		Search: SearchScreen,
+    Account: AccountsScreen,
+    PostForm: PostFormScreen,
 	},
 	{
 		initialRouteName: "Feed",
