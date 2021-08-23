13e249d5a4aa2c61ad5b5d3eb80a73fd37bdb77a
Brian Kozub <brikozub@gmail.com>
2021-08-22 19:18:54 -0600

    Building a Color Style Guide in React Native. Changed color variables to a single color object.

---
+++ styles/colors.ts
@@ -0,0 +1,7 @@
+export default {
+	primary: "#33363a",
+	dark: "#1f2125",
+	warning: "#ac262b",
+	highlight: "#eb523c",
+	lightGrey: "#eaeaea",
+};
--- styles/navigation/bottomTabStyles.ts
+++ styles/navigation/bottomTabStyles.ts
@@ -1,8 +1,9 @@
 import { StyleSheet } from "react-native";
+import colors from "../colors"
 
 export default StyleSheet.create({
 	container: {
-		backgroundColor: "blue",
+		backgroundColor: colors.highlight,
 		paddingTop: 15,
 		paddingBottom: 15,
 	},
