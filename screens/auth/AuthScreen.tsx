4d7632ed9bdc9c78adb690329eece9c14fb0f3ec
Brian Kozub <brikozub@gmail.com>
2021-08-23 21:41:54 -0600

    React Native TextInput Component Overview.

--- screens/auth/AuthScreen.tsx
+++ screens/auth/AuthScreen.tsx
@@ -1,42 +1,52 @@
 import React, { useState } from "react";
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity, TextInput } from "react-native";
 
 export default () => {
 	const [formToShow, setFormToShow] = useState("LOGIN");
-    let textObj = {
-        bodyText: "",
-        headerText: "",
-    };
+	const [email, setEmail] = useState("");
 
-    const screenTypeText = () => {
-        if (formToShow === "LOGIN") {
-            textObj = {
-                bodyText: "Need an account? Register.",
-                headerText: "Login",
-            }
-            return textObj;
-        } else if (formToShow === "REGISTER") {
-            textObj = {
-                bodyText: "Already have an account? Login.",
-                headerText: "Register",
-            }
-            return textObj;
-        }
-    }
+	let textObj = {
+		bodyText: "",
+		headerText: "",
+	};
 
-    const handleAuthTypePress = () => {
-        if (formToShow === 'LOGIN') {
-            setFormToShow("REGISTER");
-        } else if (formToShow === 'REGISTER') {
-            setFormToShow('LOGIN')
-        }
-    }
+	const screenTypeText = () => {
+		if (formToShow === "LOGIN") {
+			textObj = {
+				bodyText: "Need an account? Register.",
+				headerText: "Login",
+			};
+			return textObj;
+		} else if (formToShow === "REGISTER") {
+			textObj = {
+				bodyText: "Already have an account? Login.",
+				headerText: "Register",
+			};
+			return textObj;
+		}
+	};
 
-    screenTypeText();
+	const handleAuthTypePress = () => {
+		if (formToShow === "LOGIN") {
+			setFormToShow("REGISTER");
+		} else if (formToShow === "REGISTER") {
+			setFormToShow("LOGIN");
+		}
+	};
+
+	screenTypeText();
 
 	return (
 		<View>
 			<Text style={{ marginTop: 50 }}>{textObj.headerText}</Text>
+
+			<View style={{ marginTop: 20, marginBottom: 20 }}>
+				<TextInput
+                placeholder={"Email"}
+					value={email}
+					onChangeText={(val) => setEmail(val)}
+				></TextInput>
+			</View>
 
 			<TouchableOpacity onPress={handleAuthTypePress}>
 				<Text>{textObj.bodyText}</Text>
