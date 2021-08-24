a67cdfdd6c4633182baef54a230b8f3d9761f998
Brian Kozub <brikozub@gmail.com>
2021-08-23 21:32:34 -0600

    Using React Hooks to Dynamically Render Content. Return an object instead of creating a second function for headerText.

--- screens/auth/AuthScreen.tsx
+++ screens/auth/AuthScreen.tsx
@@ -1,13 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, TouchableOpacity } from "react-native";
 
 export default () => {
+	const [formToShow, setFormToShow] = useState("LOGIN");
+    let textObj = {
+        bodyText: "",
+        headerText: "",
+    };
+
+    const screenTypeText = () => {
+        if (formToShow === "LOGIN") {
+            textObj = {
+                bodyText: "Need an account? Register.",
+                headerText: "Login",
+            }
+            return textObj;
+        } else if (formToShow === "REGISTER") {
+            textObj = {
+                bodyText: "Already have an account? Login.",
+                headerText: "Register",
+            }
+            return textObj;
+        }
+    }
+
+    const handleAuthTypePress = () => {
+        if (formToShow === 'LOGIN') {
+            setFormToShow("REGISTER");
+        } else if (formToShow === 'REGISTER') {
+            setFormToShow('LOGIN')
+        }
+    }
+
+    screenTypeText();
+
 	return (
 		<View>
-			<Text style={{marginTop: 50}}>Login</Text>
+			<Text style={{ marginTop: 50 }}>{textObj.headerText}</Text>
 
-			<TouchableOpacity>
-				<Text>Already have an account? Register.</Text>
+			<TouchableOpacity onPress={handleAuthTypePress}>
+				<Text>{textObj.bodyText}</Text>
 			</TouchableOpacity>
 		</View>
 	);
