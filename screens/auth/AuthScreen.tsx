9d63edc4115a3d6129a6e8014cd3b639d5db1fef
Brian Kozub <brikozub@gmail.com>
2021-08-23 22:46:35 -0600

    Implementing JWT Authentication in React Native

--- screens/auth/AuthScreen.tsx
+++ screens/auth/AuthScreen.tsx
@@ -1,9 +1,13 @@
 import React, { useState } from "react";
 import { View, Text, TouchableOpacity, TextInput } from "react-native";
+import axios from "axios";
 
 import textInputStyles from "../../styles/forms/textInputStyles";
-const {textField, textFieldWrapper} = textInputStyles;
+const { textField, textFieldWrapper } = textInputStyles;
 import authScreenStyles from "../../styles/stacks/auth/authScreenStyles";
+
+const apiEndpoint =
+	"https://brikozub.devcamp.space/memipedia/memipedia_user_token";
 
 export default () => {
 	const [formToShow, setFormToShow] = useState("LOGIN");
@@ -39,12 +43,25 @@ export default () => {
 		}
 	};
 
+	const handleSubmit = () => {
+		const postData = {
+			auth: {
+				email: email,
+				password: password,
+			},
+		};
+		axios
+			.post(apiEndpoint, postData)
+			.then((response) =>
+				console.log("Response from handleSubmit", response.data)
+			)
+			.catch((error) => console.log("Error retrieving token.", error));
+	};
+
 	screenTypeText();
 
 	return (
-		<View
-			style={authScreenStyles.container}
-		>
+		<View style={authScreenStyles.container}>
 			<Text style={{ color: "white" }}>{textObj.headerText}</Text>
 
 			<View style={textFieldWrapper}>
@@ -57,18 +74,21 @@ export default () => {
 					spellCheck={false}
 				></TextInput>
 			</View>
-            <View style={textFieldWrapper}>
+			<View style={textFieldWrapper}>
 				<TextInput
 					style={textField}
 					placeholder={"Password"}
 					value={password}
 					onChangeText={(val) => setPassword(val)}
-                    secureTextEntry = {true}
+					secureTextEntry={true}
 				></TextInput>
 			</View>
 
 			<TouchableOpacity onPress={handleAuthTypePress}>
 				<Text style={{ color: "white" }}>{textObj.bodyText}</Text>
+			</TouchableOpacity>
+            <TouchableOpacity onPress={handleSubmit}>
+				<Text style={{ color: "white" }}>{textObj.headerText}</Text>
 			</TouchableOpacity>
 		</View>
 	);
