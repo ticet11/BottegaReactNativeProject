c0370dccb34b73122d8a2e7ee45a0fd25720cca1
Brian Kozub <brikozub@gmail.com>
2021-08-29 16:20:43 -0600

    Implementing the API Call for Post Data

--- screens/FeedScreen.tsx
+++ screens/FeedScreen.tsx
@@ -1,8 +1,9 @@
-import React, { useContext } from "react";
-import { Text, TouchableOpacity, View } from "react-native";
+import React, { useState, useEffect } from "react";
+import { Text, View } from "react-native";
+import * as SecureStore from 'expo-secure-store';
 
 import Container from "../utils/components/layouts/Container";
-import CurrentUserContext from '../utils/contexts/CurrentUserContext';
+import api, { secureToken } from "../utils/api";
 
 interface IFeedScreenProps {
 	navigation: {
@@ -10,11 +11,33 @@ interface IFeedScreenProps {
 	};
 }
 export default (props: IFeedScreenProps) => {
-	const {currentUser} = useContext(CurrentUserContext);
+	const [posts, setPosts] = useState([]);
+
+	useEffect(() => {
+		const ac = new AbortController();
+		getPosts();
+		return () => ac.abort();
+	}, []);
+
+	const getPosts = async () => {
+		const token = await SecureStore.getItemAsync(secureToken);
+
+		api.get("memipedia_posts", {
+			headers: {
+				Authorization: `Bearer ${token}`,
+			},
+		}).then(response => {
+			console.log('res from posts', response.data)
+			setPosts(response.data.memipedia_posts);
+		}).catch(error => {
+			console.log(error + ": Uh oh post get problems")
+		});
+	};
+
 	return (
 		<Container navigate={props.navigation.navigate}>
 			<Text>Feed screen</Text>
-			<View style={{marginTop: 20}}><Text>{JSON.stringify(currentUser)}</Text></View>
+			<View style={{ marginTop: 20 }}><Text>{JSON.stringify(posts)}</Text></View>
 		</Container>
 	);
 };
