7b2346361ed37e6e7bf255f2c6a2243bb5637ead
Brian Kozub <brikozub@gmail.com>
2021-08-29 18:32:08 -0600

    How to Customize the Status Bar Color in React Native

--- App.tsx
+++ App.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { createAppContainer } from "react-navigation";
+import { StatusBar } from "react-native";
+
 import CurrentUserProvider from "./utils/providers/CurrentUserProvider";
 
 import Router from "./utils/Router";
@@ -9,6 +11,7 @@ const AppContainer = createAppContainer(Router);
 export default function App() {
 	return (
 		<CurrentUserProvider>
+			<StatusBar barStyle="light-content"></StatusBar>
 			<AppContainer />
 		</CurrentUserProvider>
 	);
