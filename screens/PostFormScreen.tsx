43ccb2e4323b12fbd5478b6f73f38bcb1bf93c04
Brian Kozub <brikozub@gmail.com>
2021-08-29 18:39:28 -0600

    Building the Initial Post Form

--- screens/PostFormScreen.tsx
+++ screens/PostFormScreen.tsx
@@ -1,15 +1,33 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useState } from "react";
+import { Text, TextInput, View } from "react-native";
+import Button from "../utils/components/helpers/Button";
 
 import PostImagePicker from "../utils/components/posts/PostImagePicker";
 
 export default () => {
+	const [name, setName] = useState("");
+	const [content, setContent] = useState("");
 	return (
 		<View>
-			<Text>Post Form Screen</Text>
+			<TextInput
+				placeholder="Name"
+				value={name}
+				onChangeText={(val) => setName(val)}
+			></TextInput>
+			<TextInput
+				multiline
+				placeholder="Description"
+				value={content}
+				onChangeText={(val) => setContent(val)}
+				style={{ borderWidth: 2, borderColor: "black" }}
+			></TextInput>
 			<View style={{ marginTop: 40, height: 100 }}>
 				<PostImagePicker></PostImagePicker>
 			</View>
+			<Button
+				text="Submit"
+				onPress={() => console.log("Very good submit. I like it.")}
+			></Button>
 		</View>
 	);
 };
