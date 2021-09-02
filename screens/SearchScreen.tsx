012b2c3560ad3a099ba802bb95978586155069a6
Brian Kozub <brikozub@gmail.com>
2021-09-01 19:07:00 -0600

    Creating the Initial Search Bar

--- screens/SearchScreen.tsx
+++ screens/SearchScreen.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { View, Text, TextInput, TouchableOpacity } from "react-native";
 
 import Container from "../utils/components/layouts/Container";
@@ -6,36 +6,30 @@ import Container from "../utils/components/layouts/Container";
 interface ISearchScreenProps {
 	navigation: {
 		navigate: (arg: string) => void;
-	}
+	};
 }
 
 export default (props: ISearchScreenProps) => {
-	const [query, setQuery] = useState('');
+	const [query, setQuery] = useState("");
 
 	const handleSearch = () => {
-		console.log('searching for ' + query);
-	}
+		console.log("searching for " + query);
+	};
 
 	const searchBar = (
 		<View>
 			<TextInput
-			value={query}
-			onChangeText={val => setQuery(val)}
-			placeholderTextColor="white"
-			placeholder="Search Query"
-			onSubmitEditing={()=> console.log("Searching ...")}
+				value={query}
+				onChangeText={(val) => setQuery(val)}
+				placeholderTextColor="white"
+				placeholder="Search Query"
+				onSubmitEditing={handleSearch}
 			/>
-			<TouchableOpacity
-				onPress={handleSearch}
-			>
-				<Text
-				style={{color: "white"}}
-				>
-					Search
-				</Text>
+			<TouchableOpacity onPress={handleSearch} style={{marginTop: 20}}>
+				<Text style={{ color: "white" }}>Search</Text>
 			</TouchableOpacity>
 		</View>
-	)
+	);
 
 	return (
 		<Container navigate={props.navigation.navigate}>
